48f589e39957056d575bcea428c093e73bd18d5b
Rebecca Jennings <157240153+Rebecca-J7@users.noreply.github.com>
2026-04-04 15:59:38 -0700

    debugging

--- restroom-supply-map/app/page.tsx
+++ restroom-supply-map/app/page.tsx
@@ -1,6 +1,10 @@
+// app/page.tsx
 "use client"
 
 import MapComponent from "../components/Map"
+
+// Force Next.js to render this page on the client at runtime
+export const dynamic = "force-dynamic"
 
 export default function Home() {
   return <MapComponent />
--- restroom-supply-map/components/Map.tsx
+++ restroom-supply-map/components/Map.tsx
@@ -1,12 +1,7 @@
-"use client"
-
-// components/Map.tsx
 import dynamic from "next/dynamic"
-import "../styles/map.css"
 
 const MapWithNoSSR = dynamic(() => import("./MapClient"), { ssr: false })
 export default MapWithNoSSR
-
 // // Dynamically import MapContainer component with no SSR
 // const MapWithNoSSR = dynamic(
 //   () => import("./MapClient"), // separate client-only map component
